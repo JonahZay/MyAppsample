5bc3c931f5d18cf5ad2fe484211f3c58f95759b6
Jonah Zays <aquinojonathanpc@outlook.com>
2019-02-25 14:48:24 -0800

    another change

---
+++ apple.js
@@ -0,0 +1,67 @@
+// function a(){
+//   var jonathanDay=24;
+//   console.log(jonathanDay);
+// year();
+// }
+// function b(){
+//   var jonathanMonth = 09;
+//   console.log(jonathanMonth);
+//   a();
+// }
+// function year(){
+//   var jonathanYear = 1999;
+//   console.log(jonathanYear);
+// }
+//
+//
+//
+//
+// b();
+console.log('hello');
+
+
+// function waitThreeSeconds (){
+//   var ms =3000 + new Date().getTime();
+//   while(new Date() < ms){}
+//     console.log('finished function');
+//
+//   }
+//   function clickHandler(){
+//     console.log('click event!');
+//
+//   }
+// document.addEventListener('click', clickHandler);
+//
+// waitThreeSeconds();
+// console.log('finished execution')v
+// function a() {
+//
+//   var cust = {
+//     firstname: 'jonathan',
+//     lastname: 'aquino',
+//     address: {
+//       state: 'texas',
+//       city: 'grapevine',
+//       street: '3019 mustang drive',
+//       apt: '209'
+//     }
+//   }
+//   console.log(' This customer is a teenager\n',cust);
+// }
+// function customer133() {
+//   a();
+// }
+//
+//
+//
+//
+//
+//
+//
+//
+//
+// customer133();
+
+
+// precedence number and operations(false === 0)=false
+//coercion a= 1+"2"== 12 number + string =corecion
